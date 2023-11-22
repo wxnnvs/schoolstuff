5af55f9a65aa40c216c76a3e175c1a929e79aad5
wxnnvs <215158@proton.me>
2023-11-22 11:49:17 +0100

    fix dubblejoeris

--- 3NWSTb/2023-2024/INFORMATICA/4/4.5/ScherpofRechtofStomp.py
+++ 3NWSTb/2023-2024/INFORMATICA/4/4.5/ScherpofRechtofStomp.py
@@ -4,9 +4,9 @@ kleinste = float(input("kleinste zijde:    "))
 middelste = float(input("middelste zijde:   "))
 grootste = float(input("grootste zijde:    "))
 
-if sqrt((kleinste**2)+(middelste**2))<grootste:
+if sqrt((kleinste**2)+(middelste**2))<grootste**2:
     print("Stompe driehoek")
-elif sqrt((kleinste**2)+(middelste**2))>grootste:
+elif sqrt((kleinste**2)+(middelste**2))>grootste**2:
     print("Scherpe driehoek")
 else:
     print("Rechte driehoek")
