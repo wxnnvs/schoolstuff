5519ceb9099d8060c9382efd72a43b6e09fcbca6
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-01-10 11:38:40 +0100

    fix

--- 3ejaars/2023-2024/INFORMATICA/4/4.5/soldenkorting.py
+++ 3ejaars/2023-2024/INFORMATICA/4/4.5/soldenkorting.py
@@ -6,8 +6,8 @@ elif stuks==2 or stuks==3:
     print("20%")
 elif stuks==4:
     print("30%")
-elif stuks<=5:
-    if float(input("hoeveel euro kostte dit?  "))<=300:
+elif stuks>=5:
+    if float(input("hoeveel euro kostte dit?  "))>=300:
         print("50%")
     else:
         print("40%")
